f7fea0aad0f2e70fbbe28dd91da81f7996f7f451
prastutiupadhyay33 <prastutiupadhyay33@gmail.com>
2026-06-04 10:00:30 +0530

    Update and rename Assignment-G (SQL).sql to Assignment-G (Stored Procedure) (SQL).sql

--- Assignment-G (Stored Procedure) (SQL).sql
+++ Assignment-G (Stored Procedure) (SQL).sql
@@ -12,62 +12,82 @@ insert into employyes (id,name,department,salary) values
 (3, 'Charlie','IT',60000),
 (4, 'David','HR',55000),
 (5, 'EVe','Finance',65000);
-# Create a procedure to show all employees details.
+
+#1 Create a procedure to show all employees details.
+
 Delimiter // 
 create procedure showallemployyes()Begin select*from employyes;
 End //
 Delimiter ;
 Call showallemployyes();
-# Create a stored procedure to fetch all employees from a specific department.
+
+#2 Create a stored procedure to fetch all employees from a specific department.
+
 Delimiter // 
 create procedure getemployyesByDept(In dept_name varchar(50))Begin select*from employyes where department=dept_name;
 End //
 Delimiter ;
 Call getemployyesByDept('IT');
-# Create a stored procedure to increase salary by a given percentage for a department.
+
+#3 Create a stored procedure to increase salary by a given percentage for a department.
+
 Delimiter // 
 create procedure IncreaseSalaryByDepty(In dept_name VARCHAR(50),In percent_increase Decimal(5,2))Begin update employyes set salary=salary+(salary*percent_increase/100) where department=dept_name;
 End //
 Delimiter ;
 set sql_safe_updates=0;
 Call IncreaseSalaryByDepty('IT',10);
-# Create a procedure to return the total salary of all employees.
+
+#4 Create a procedure to return the total salary of all employees.
+
 Delimiter //
 create procedure totalsalary() Begin select sum(salary) from employyes;
 End //
 Delimiter ;
 Call totalsalary(); 
-# Create a procedure to insert a new employee into an existing table.
+
+#5 Create a procedure to insert a new employee into an existing table.
+
 Delimiter //
 create procedure addemployyes( In emp_name Varchar(50),In emp_dept Varchar(50),In emp_salary INT) Begin insert into employyes(name,department,salary) values (emp_name,emp_dept,emp_salary);
 End //
 Delimiter ;
 Call addemployyes('Frank','Finance',62000);
-# Create a procedure to delete an existing employee from the table by id.
+
+#6 Create a procedure to delete an existing employee from the table by id.
+
 Delimiter //
 create procedure deleteemployesbyid( In empid INT) Begin delete from employyes where empid=id;
 End //
 Delimiter ;
 Call deleteemployesbyid(3);
-# Create a procedure to return average salary.
+
+#7 Create a procedure to return average salary.
+
 Delimiter //
 create procedure returnavgsalary() Begin select avg(salary) from employyes;
 End //
 Delimiter ;
 Call returnavgsalary();
-# Create a procedure to get employees above a salary.
+
+#8 Create a procedure to get employees above a salary.
+
 Delimiter // 
 create procedure getemployyesabovesalary(In salary_emp int)Begin select*from employyes where salary>salary_emp;
 End //
 Delimiter ;
 Call getemployyesabovesalary(50000);
-# Create a procedure to get highest salary.
+
+#9 Create a procedure to get highest salary.
+
 Delimiter // 
 create procedure highestsalary()Begin select max(salary) from employyes;
 End //
 Delimiter ;
 Call highestsalary();
-# Create a procedure to update employee name.
+
+#10 Create a procedure to update employee name.
+
 DELIMITER //
 create procedure updateemployees(IN emp_name varchar(50),in id_emp int) begin update employyes set name=emp_name where id_emp=id;
 END //
